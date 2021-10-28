4cc64abef22ecbd0a6ead7c614a7da202c194b36
Aniket Pal <aniketindian8@gmail.com>
2021-10-28 22:27:18 +0530

    Feat: bin functions to compile cpp andpython added

---
+++ bin/excecutePy.ts
@@ -0,0 +1,16 @@
+import { exec } from "child_process";
+
+const executePy = (filepath: string) => {
+  return new Promise((resolve, reject) => {
+    // exec runs 2 child process together
+    exec(`python ${filepath}`, (error, stdout, stderr) => {
+      error && reject({ error, stderr });
+      stderr && reject(stderr);
+      resolve(stdout);
+    });
+  });
+};
+
+module.exports = {
+  executePy,
+};
