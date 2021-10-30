7960420a22436ef83255c1c4422c1036c03fa522
Aniket Pal <aniketindian8@gmail.com>
2021-10-30 17:49:02 +0530

    Chore: colors and style configured for app

---
+++ app/config/colors.ts
@@ -0,0 +1,10 @@
+export default {
+    primary: '#fff',
+    secondary: '#fff',
+    black: '#000',
+    white: '#fff',
+    medium: '#fff',
+    light: '#fff',
+    dark: '#fff',
+    danger: '#fff',
+}
