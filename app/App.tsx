5e1331a8b45a1ad328322c1c230f3692291acfa9
Aabhas <aabhassao0@gmail.com>
2021-10-30 18:45:38 +0530

    Feat: Opportunities intitialized
    Signed-off-by: Aabhas <aabhassao0@gmail.com>

--- app/App.tsx
+++ app/App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Firebase from './Firebase';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -17,6 +18,7 @@ export default function App() {
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
+        <Firebase />
       </SafeAreaProvider>
     );
   }
---
+++ app/screens/opportunities/opportunity.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from '../../components/Themed';
+
+export default function Opportunity() {
+    return (
+        <View>
+            <Text>opp</Text>
+        </View>
+    );
+}
