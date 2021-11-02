a8727b2af01c974bb06887f96d84ec83084dfab1
Aabhas <aabhassao0@gmail.com>
2021-11-02 13:04:15 +0530

    Update: Opportunity Card UI
    Signed-off-by: Aabhas <aabhassao0@gmail.com>

--- app/screens/opportunities/opportunity.tsx
+++ app/screens/opportunities/opportunity.tsx
@@ -1,30 +1,44 @@
 import React from 'react';
-import { Text, View } from '../../components/Themed';
-import { Image } from 'react-native';
+import { Text } from '../../components/Themed';
 import OpportunityType from './opportunityType';
 import styled from '@emotion/native';
-import { SIZES } from '../../constants';
 
-const Box = styled.View`
+const OpportunityCard = styled.View`
     display: flex;
     flex-direction: column;
-    height: 200px;
-    width: 50%;
+    justify-content: space-evenly;
+    height: 250px;
+    width: 40%;
     align-items: center;
-    background-color: beige;
+    border-radius: 12px;
+    background-color: gray;
 `;
 
+const CircularImage = styled.Image`
+    width: 80px;
+    height: 80px;
+    border-radius: 60px;
+`;
+
+const OppurtunityContent = styled.View`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    height: 100px;
+    align-items: center;
+`;
 
 const Opportunity: React.FC<OpportunityType> =  ({ url, company, role, duration, deadline}) => {
     return (
-        <Box>
-            <Image source = {{uri: url}} />
-            <Text>opp</Text>
-            <Text> { company } </Text>
-            <Text> { role } </Text>
-            <Text> { duration } months </Text>
-            <Text> { deadline } </Text>
-        </Box>
+        <OpportunityCard>
+            <CircularImage source={{uri: url}} />
+            <OppurtunityContent>
+                <Text> { company } </Text>
+                <Text> { role } </Text>
+                <Text> { duration } months </Text>
+                <Text> { deadline } </Text>
+            </OppurtunityContent>
+        </OpportunityCard>
     );
 }
 
