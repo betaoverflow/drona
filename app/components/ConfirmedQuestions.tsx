42f40496ebc0b8f060c92d5f27229e9069728321
Sumana Basu <sumana.basu2001@gmail.com>
2021-11-04 00:28:08 +0530

    Enhancement: Questionnaire styling and scroll view

--- app/components/ConfirmedQuestions.tsx
+++ app/components/ConfirmedQuestions.tsx
@@ -5,6 +5,7 @@ import { View, Text } from './Themed'
 import { Transition, Transitioning } from 'react-native-reanimated'
 import { MaterialIcons } from '@expo/vector-icons'
 import { StyleSheet, TouchableOpacity } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 
 const transition = (
     <Transition.Together>
@@ -29,44 +30,47 @@ function ConfirmedQuestions() {
         }
     }, [])
     return (
-        <Transitioning.View ref={ref} transition={transition}>
-            <View style={styles.container}>
-                {questions.map(question => {
-                    if (question.isApproved == true) {
-                        return (
-                            <View key={question._id}>
-                                <View style={styles.card}>
-                                    <Text>{question.title}</Text>
-                                    <TouchableOpacity
-                                        key={question._id}
-                                        onPress={() => {
-                                            if (ref != null) {
-                                                if (ref.current != null) {
-                                                    ref.current.animateNextTransition()
-                                                    setCurrentIndex(question._id === currentIndex ? null : question._id)
+        <ScrollView>
+            <Transitioning.View ref={ref} transition={transition}>
+                <View style={styles.container}>
+                    {questions.map(question => {
+                        if (question.isApproved == true) {
+                            return (
+                                <View key={question._id} style={styles.cardContainer}>
+                                    <View style={styles.card}>
+                                        <Text style={styles.heading}>{question.title}</Text>
+                                        <TouchableOpacity
+                                            key={question._id}
+                                            onPress={() => {
+                                                if (ref != null) {
+                                                    if (ref.current != null) {
+                                                        ref.current.animateNextTransition()
+                                                        setCurrentIndex(question._id === currentIndex ? null : question._id)
+                                                    }
                                                 }
-                                            }
-                                        }}
-                                        activeOpacity={0.9}
-                                    >
-                                        <MaterialIcons
-                                            name={question._id === currentIndex ? 'keyboard-arrow-up' : 'keyboard-arrow-down'}
-                                            color="#fff"
-                                            size={30}
-                                        />
-                                    </TouchableOpacity>
-                                </View>
-                                {question._id === currentIndex && (
-                                    <View>
-                                        <Text>{question.message}</Text>
+                                            }}
+                                            activeOpacity={0.9}
+                                        >
+                                            <MaterialIcons
+                                                name={question._id === currentIndex ? 'keyboard-arrow-up' : 'keyboard-arrow-down'}
+                                                color="#fff"
+                                                size={30}
+                                            />
+                                        </TouchableOpacity>
                                     </View>
-                                )}
-                            </View>
-                        )
-                    }
-                })}
-            </View>
-        </Transitioning.View>
+
+                                    {question._id === currentIndex && (
+                                        <View style={styles.block}>
+                                            <Text>{question.message}</Text>
+                                        </View>
+                                    )}
+                                </View>
+                            )
+                        }
+                    })}
+                </View>
+            </Transitioning.View>
+        </ScrollView>
     )
 }
 
@@ -75,26 +79,29 @@ export default ConfirmedQuestions
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
+        marginTop: 20,
+        marginBottom: 60,
+    },
+    cardContainer: {
+        minWidth: '100%',
+        padding: 10,
     },
     card: {
         flexGrow: 1,
+        backgroundColor: '#2E2D2D',
         alignItems: 'center',
-        justifyContent: 'center',
         flexDirection: 'row',
+        padding: 20,
+        justifyContent: 'space-between',
+        height: 80,
+    },
+    block: {
+        backgroundColor: '#2E2D2D',
+        padding: 20,
     },
     heading: {
-        fontSize: 38,
-        fontWeight: '900',
-        textTransform: 'uppercase',
-        letterSpacing: -2,
-    },
-    body: {
-        fontSize: 20,
-        lineHeight: 20 * 1.5,
-        textAlign: 'center',
-    },
-    subCategoriesList: {
-        marginTop: 20,
+        fontSize: 18,
+        fontWeight: 'bold',
     },
 })
