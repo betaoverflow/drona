7ca7f0e0a443bc663e2b95a97d81c530020e9d51
Aabhas <aabhassao0@gmail.com>
2021-11-03 12:59:58 +0530

    Update: Remove Multer and disk storage of code files
    Signed-off-by: Aabhas <aabhassao0@gmail.com>

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from 'express'
-import multer from 'multer'
 
 const { generateFile } = require('../utils/generateFile')
 const { executeCpp } = require('../bin/excecuteCpp')
@@ -9,22 +8,6 @@ var router = express.Router()
 
 router.get('/', (req: Request, res: Response) => {
     return res.json({ TeamName: 'Betaoverflow' })
-})
-
-const fileName = (__dirname + '/code.jpeg') as string
-
-// dummy function
-// actual: run detection algo and get text
-function detectText(filename: any) {
-    console.log(filename)
-}
-
-router.get('/code', (req: Request, res: Response) => {
-    async function valued() {
-        const value = await detectText(fileName)
-        return res.json({ code: value })
-    }
-    valued()
 })
 
 router.post('/run', async (req: Request, res: Response) => {
@@ -56,34 +39,6 @@ router.post('/run', async (req: Request, res: Response) => {
     } catch (err) {
         res.status(500).json({ error: Error })
     }
-})
-
-//callbacks for storing to Disk Storage
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'routes/')
-    },
-    filename: function (req, file, cb) {
-        cb(null, 'code.jpeg')
-    },
-})
-
-//Checking if file is of the allowed types
-const fileFilter = (req: Request, file: File, cb: any) => {
-    const allowedFileTypes = ['image/jpeg', 'image/jpg', 'image/png']
-    if (allowedFileTypes.includes(file.type)) {
-        cb(null, true)
-    } else {
-        cb(null, false)
-    }
-}
-
-//saving single file to Disk Storage
-let upload = multer({ storage, fileFilter: express })
-
-router.post('/add', upload.single('myFile'), function (req, res, next) {
-    // req.file is the `myFile` file
-    // req.body will hold the text fields, if there were any
 })
 
 module.exports = router
