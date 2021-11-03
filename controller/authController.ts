d2d5743ee0fb36a016a061c756108d559ce77a27
Sumana Basu <sumana.basu2001@gmail.com>
2021-11-03 12:13:31 +0530

    Feat: Auth controller and auth model fixed

--- controller/authController.ts
+++ controller/authController.ts
@@ -6,19 +6,19 @@ import jwt from 'jsonwebtoken'
 
 var router = express.Router()
 var ObjectId = require('mongoose').Types.ObjectId
-var { User } = require('../models/user')
+var User = require('../models/user')
 
 router.post('/register', async (req: Request, res: Response) => {
-    log.info(req.body)
+    console.log(req.body)
     try {
-        const newPassword = bcrypt.hash(req.body.password, bcp.salt)
+        const newPassword = await bcrypt.hash(req.body.password, 10)
         await User.create({
-            email: req.body.name,
+            email: req.body.email,
             password: newPassword,
         })
         res.json({ status: 'ok' })
     } catch (err) {
-        res.json({ status: 'error', error: 'Duplicate email' })
+        res.json({ status: 'error', error: err })
     }
 })
 
@@ -28,7 +28,7 @@ router.post('/login', async (req: Request, res: Response) => {
     })
 
     if (!user) {
-        return { status: 'error', error: 'Invalid Login' }
+        return { status: 'error', error: 'Invalid login' }
     }
 
     const isPasswordValid = await bcrypt.compare(req.body.password, user.password)
@@ -36,10 +36,12 @@ router.post('/login', async (req: Request, res: Response) => {
     if (isPasswordValid) {
         const token = jwt.sign(
             {
+                name: user.name,
                 email: user.email,
             },
-            bcp.secret
+            'secret123'
         )
+
         return res.json({ status: 'ok', user: token })
     } else {
         return res.json({ status: 'error', user: false })
--- models/user.ts
+++ models/user.ts
@@ -1,15 +1,13 @@
-import mongoose from 'mongoose'
+const mongooseUser = require('mongoose')
 
-const User = new mongoose.Schema(
+const User = new mongooseUser.Schema(
     {
-        email: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
     },
-    {
-        collection: 'user-data',
-    }
+    { collection: 'user-data' }
 )
 
-const model = mongoose.model('UserData', User)
+const model = mongooseUser.model('UserData', User)
 
 module.exports = model
