2f8544a7f3a8078af3303d153e7a2d7cd365a7c1
kumarshantanu01 <shantanuk280@gmail.com>
2021-11-04 14:53:45 +0530

    feat: about page enhanced

--- web/components/home/About.tsx
+++ web/components/home/About.tsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import styles from '../../styles/about.module.scss';
+
+const Aboutcard = (props) => {
+    return(
+        <div className={styles.aboutcard}>
+            <div className={styles.text}>
+                <div className={styles.heading}>{props.Heading}</div>
+                <div className={styles.heading}>{props.Subheading}</div>
+            </div>
+            <div className={styles.logo}>
+                {props.logo}
+            </div>
+        </div>
+    )
+}
+
+const Aboutcarddata=[
+    {
+        Heading:"Run Your Hand-Written Code",
+        Subheading:"Run your hand-written code and code on your phone in three clicks",
+        logo: "logo",
+    },
+
+    {
+        Heading:"Practice from unlimited questions.",
+        Subheading:"Run your hand-written code and code on your phone in three clicks",
+        logo: "logo",
+    },
+
+    {
+        Heading:"Get constant updates",
+        Subheading:"Run your hand-written code and code on your phone in three clicks",
+        logo: "logo",
+    },
+]
 
 const About = () => {
     return(
         <>
-        About
+        <div className={styles.about}>
+            <div className={styles.heading}>What it does</div>
+            <div className={styles.aboutcards}>
+                {Aboutcarddata.map(function ncards(val){
+                return(
+                    <Aboutcard
+                    Heading={val.Heading}
+                    Subheading={val.Subheading}
+                    logo={val.logo}
+                    />
+                );
+                })}
+            </div>
+        </div>
         </>
     )
 }
