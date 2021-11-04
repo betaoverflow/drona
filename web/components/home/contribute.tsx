9cee3f81006f461b6933465387b79b64a278e095
Aniket Pal <aniketindian8@gmail.com>
2021-11-04 14:55:45 +0530

    Feat: Restruct home

---
+++ web/components/home/contribute.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from '../../styles/Home.module.scss'
+
+const HomeContribute: React.FC = () => {
+    return <h1>Be the senior you wanted as a junior</h1>
+}
+
+export default HomeContribute
