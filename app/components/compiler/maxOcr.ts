2f1ae95f5d27f5edc6a6433bd31d59a909a5ed48
Aabhas <aabhassao0@gmail.com>
2021-11-04 14:00:42 +0530

    Feat: Upload file from Image Picker to OCR
    Signed-off-by: Aabhas <aabhassao0@gmail.com>

---
+++ app/components/compiler/maxOcr.ts
@@ -0,0 +1,28 @@
+import { MAX_OCR_ADDRESS } from "../constants/maxOcrAddress"
+import RNFetchBlob from "rn-fetch-blob";
+
+const textFromArray = (inp: any) => {
+    let res = "";
+    for (let i = 0; i < inp.length; i++) {
+        res += inp[i] + '\n';
+    }
+
+    return res;
+}
+
+export const getTextFromMaxOcr = async (filepath: string) => {
+    try {
+        const res = await RNFetchBlob.fetch('POST', `${MAX_OCR_ADDRESS}/model/predict`, {
+            'Content-Type': 'multipart/form-data'
+        }, [
+            {name: 'image', filename: 'image.png', data: RNFetchBlob.wrap(filepath)}
+        ])
+
+        const ans = JSON.parse(res.data);
+        console.log(textFromArray(ans.text));
+
+
+    } catch(e) {
+        console.log(e, ' error sending model data to ocr');
+    }
+}
--- app/screens/CompilerScreen.tsx
+++ app/screens/CompilerScreen.tsx
@@ -5,40 +5,58 @@ import { TextInput } from 'react-native-gesture-handler'
 import { Picker } from '@react-native-picker/picker'
 import { Button } from 'react-native'
 import {CameraOptions, launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import { getTextFromMaxOcr } from '../components/compiler/maxOcr'
+import { readFile } from 'react-native-fs'
 
 const Editor = () => {
     const [code, setCode] = useState('')
     const [output, setOutput] = useState('')
     const [lang, setLang] = useState('cpp')
+    const [filepath, setFilepath] = useState(undefined)
 
     const handleSubmit = async () => {
-        const payload = JSON.stringify({
-            lang: lang,
-            code,
-        })
-
-        try {
-            const { data } = await axios.post('http://localhost:8080/run', payload, {
-                headers: { 'Content-Type': 'application/json' },
-            })
-            setOutput(data.output)
-        } catch ({ response }) {
-            if (response) {
-                const errMsg = response.data.err.stderr
-                setOutput(errMsg)
-            } else {
-                setOutput('Error connecting to server!')
-            }
+        
+        // Send image file to OCR and get result
+        if (filepath) {
+            getTextFromMaxOcr(filepath!);
+    
+            // const payload = JSON.stringify({
+            //     lang: lang,
+            //     code,
+            // })
+    
+            // try {
+            //     const { data } = await axios.post('http://localhost:8080/run', payload, {
+            //         headers: { 'Content-Type': 'application/json' },
+            //     })
+            //     setOutput(data.output)
+            // } catch ({ response }) {
+            //     if (response) {
+            //         const errMsg = response.data.err.stderr
+            //         setOutput(errMsg)
+            //     } else {
+            //         setOutput('Error connecting to server!')
+            //     }
+            // }
         }
     }
 
     const cameraOptions:CameraOptions = {
         saveToPhotos: false,
-        mediaType: 'photo'
+        mediaType: 'photo',
     }
 
     const imgCallback = (res: any) => {
         console.log(res);
+        if ('didCancel' in res) {
+            console.log('cancelled by user');
+        } else if ('errorCode' in res) {
+            console.log(res.errorCode);
+        } else if ('errorMessage' in res) {
+            console.log(res.errorMessage);
+        } else {
+            setFilepath(res.assets[0].uri);
+        }
     }
 
     return (
@@ -46,6 +64,9 @@ const Editor = () => {
             <TextInput style={{ color: '#232323' }} multiline={true} numberOfLines={4} value={code} onChangeText={code => setCode(code)} />
             <Button onPress={props => launchCamera(cameraOptions, imgCallback)} title="Launch Camera" />
             <Button onPress={props=> launchImageLibrary(cameraOptions, imgCallback)} title="Select from gallery" />
+            {
+                !filepath ? <Text>No file</Text>: <Text> {filepath} </Text>
+            }
             <View>
                 <Picker selectedValue={lang} onValueChange={(value, index) => setLang(value)} mode="dropdown">
                     <Picker.Item label="C++" value="cpp" />
