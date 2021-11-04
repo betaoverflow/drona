c9daf8577f7418fc981a1d965529b84c8784fa2e
kumarshantanu01 <shantanuk280@gmail.com>
2021-11-04 17:41:56 +0530

    feat: info page added

--- web/components/home/index.tsx
+++ web/components/home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../Header'
 import { ThemeProvider } from '../themeProvider'
 import About from './About'
+import Info from './info'
 import HomeContribute from './contribute'
 import Footer from './footer'
 import Hero from './hero'
@@ -12,6 +13,7 @@ const HomeContainer: React.FC = () => {
             <Header />
             <Hero />
             <About />
+            <Info/>
             <HomeContribute />
             <Footer />
         </>
---
+++ web/components/home/info.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Info = () => {
+    return (
+        <>
+            How it works
+        </>
+    )
+}
+
+export default Info;
