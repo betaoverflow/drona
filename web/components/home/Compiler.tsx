f95589b21461b7c0106e6bc3a4f226a6091c237b
kumarshantanu01 <shantanuk280@gmail.com>
2021-11-07 14:36:18 +0530

    feat: contribute section enhanced

--- web/components/home/Compiler.tsx
+++ web/components/home/Compiler.tsx
@@ -4,7 +4,7 @@ import styles from '../../styles/compiler.module.scss'
 const Compiler = () => {
     return(
     <>
-    Compiler
+    
     </>
     )
 }
--- web/components/home/contribute.tsx
+++ web/components/home/contribute.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
-import styles from '../../styles/Home.module.scss'
+import styles from '../../styles/question.module.scss'
 
 const HomeContribute: React.FC = () => {
-    return <h1>Be the senior you wanted as a junior</h1>
+    return (
+    <>
+        <div className={styles.contribute}>
+            <div className={styles.container}>
+                <div className={styles.text}>
+                    <h1>Be the senior you wanted<br/>as a junior</h1>
+                </div>
+                <div className={styles.image}>
+                    <div className={styles.arrow}><img src='https://res.cloudinary.com/dguy8qpzi/image/upload/v1636274945/Arrow_xujefk.svg'></img></div>
+                    <div className={styles.card}><img src='https://res.cloudinary.com/dguy8qpzi/image/upload/v1636274948/Your_Photo_Here_mdjfdb.png'></img></div>
+                </div>
+            </div>
+        </div>
+    </>
+    )
 }
 
 export default HomeContribute
