c1165fd9481f42f01ea43798c5d1295b824b3ed7
kumarshantanu01 <shantanuk280@gmail.com>
2021-11-07 15:18:55 +0530

    feat: contribute section styled and responsive

--- web/components/home/contribute.tsx
+++ web/components/home/contribute.tsx
@@ -11,7 +11,7 @@ const HomeContribute: React.FC = () => {
                 </div>
                 <div className={styles.image}>
                     <div className={styles.arrow}><img src='https://res.cloudinary.com/dguy8qpzi/image/upload/v1636274945/Arrow_xujefk.svg'></img></div>
-                    <div className={styles.card}><img src='https://res.cloudinary.com/dguy8qpzi/image/upload/v1636274948/Your_Photo_Here_mdjfdb.png'></img></div>
+                    <div className={styles.card}><img src='https://res.cloudinary.com/dguy8qpzi/image/upload/v1636277071/Your_Photo_Here_skgxfn.svg'></img></div>
                 </div>
             </div>
         </div>
