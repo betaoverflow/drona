09c438770cd6071e70bf0a92364e1c97821734ef
Aniket762 <aniketindian8@gmail.com>
2021-11-07 23:51:15 +0530

    login screen bugs

--- app/config/firebase.ts
+++ app/config/firebase.ts
@@ -1,11 +1,5 @@
 
 
 export const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID,
-    appId: process.env.REACT_APP_APPID,
-    measurementId: process.env.REACT_MEASUREMENTID,
+    
 }
--- app/screens/LoginScreen.jsx
+++ app/screens/LoginScreen.jsx
@@ -1,14 +1,40 @@
-import { Container , Item  } from 'native-base'
-import React from 'react'
-import Form from "native-base"
-import { Text, View } from '../components/Themed'
+import React, { useState } from 'react'
+import { TextInput   } from 'react-native-gesture-handler'
+import { Button} from "react-native";
+import { View } from '../components/Themed';
 
 
 export default function LoginScreen()
-{
+{   
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+   
+
     return (
-        <View>
-            <Text>Hiiii</Text>
-        </View>
+        <>
+            <TextInput
+                placeholder="Enter your email"
+                value = {email}
+                onChange={e=>setEmail(e.target.value)}
+            />
+            <TextInput
+                placeholder="Enter your password"
+                value={password}
+                onChange={e=>setPassword(e.target.value)}
+            />
+            <Button
+                title="Login"
+                onPress={() => console.log("hi")}
+                color = "green"
+            >
+            </Button>
+            <View style={{height:30}} />
+            <Button
+                title="Register"
+                onPress={() => console.log("hi")}
+                color="blue"
+            ></Button>
+        </>
     )
 }
