349593cdc9e4bbb8440f78aa8b67f2b2b4676e12
Aniket Pal <aniketindian8@gmail.com>
2021-11-11 21:12:38 +0530

    Feat: Question verify route private

--- web/pages/dashboard/questionVerify.tsx
+++ web/pages/dashboard/questionVerify.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import { useEffect } from 'react'
+import axios from 'axios'
+import { useState } from 'react'
 
 export default function questionContribute() {
-    useEffect(() => {})
+    const [questions, setQuestions] = useState<any[]>([])
+
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            try {
+                axios.get(`http://localhost:8080/api/questions`).then(function (response) {
+                    // handle success
+                    setQuestions(response.data)
+                })
+            } catch (err) {
+                console.log(err)
+            }
+        } else {
+            alert('You are not logged in')
+            window.location.href = '/href'
+        }
+    })
 
     return (
         <div>
