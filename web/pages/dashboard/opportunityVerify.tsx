bdfaabf75114f05a9f08f65efb4e67cf1bdaf7e0
Aniket Pal <aniketindian8@gmail.com>
2021-11-11 21:20:37 +0530

    Feat: Opportunity verify route private

--- web/pages/dashboard/opportunityVerify.tsx
+++ web/pages/dashboard/opportunityVerify.tsx
@@ -1,6 +1,26 @@
+import axios from 'axios'
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 export default function opportunityVerify() {
+    const [opportunity, setOpportunity] = useState<any[]>([])
+
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            try {
+                axios.get(`http://localhost:8080/api/oportunity`).then(function (response) {
+                    // handle success
+                    setOpportunity(response.data)
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        } else {
+            alert('You are not logged in!')
+            window.location.href = '/admin'
+        }
+    }, [])
+
     return (
         <div>
             <h1>Opportunity Verify</h1>
