6c64151df1b7b9b45f6ed0b384653aead380e7b7
Sumana Basu <sumana.basu2001@gmail.com>
2021-11-12 20:39:05 +0530

    Feat: Hero section added

--- web/components/home/hero.tsx
+++ web/components/home/hero.tsx
@@ -3,12 +3,34 @@ import styles from '../../styles/Hero.module.scss'
 import Image from 'next/image'
 const Hero: React.FC = () => {
     return (
-        <div>
-            <div style={{ float: 'left' }}>
+        <div className={styles.heroContainer}>
+            <div className={styles.leftContainer}>
+                <h1 className={styles.heading}> Empowering everyone to code with IBM's eye</h1>
+                <p className={styles.subheading}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus velit eget magna tincidunt, nec sollicitudin neque
+                    ultrices. Sed lobortis eros sit amet venenatis accumsan. Integer sit amet sollicitudin purus. Proin fringilla iaculis
+                    felis in mollis. Donec suscipit iaculis neque, vel ultricies diam consequat non.{' '}
+                </p>
+                <div className={styles.buttons}>
+                    <div className={styles.demo}>Watch a demo</div>
+                    <div className={styles.download}>Download APK</div>
+                </div>
+            </div>
+            <div className={styles.rightContainer}>
+                <Image
+                    src="https://res.cloudinary.com/dguy8qpzi/image/upload/v1636009473/drona-homeui_xqlfkq.png"
+                    width={380}
+                    height={700}
+                    className={styles.mainImage}
+                    alt="Drona mobile app home screen"
+                />
+            </div>
+            {/* <div style={{ float: 'left' }}>
                 <h1 className={styles.heading}>
                     {' '}
                     Empowering everyone <br /> to code with IBM's eye
                 </h1>
+                <p className={styles.subheading}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus velit eget magna tincidunt,<br/> nec sollicitudin neque ultrices. Sed lobortis eros sit amet venenatis accumsan. Integer sit<br/> amet sollicitudin purus. Proin fringilla iaculis felis in mollis. Donec suscipit iaculis neque, vel<br/> ultricies diam consequat non. </p>
                 <div className={styles.buttons}>
                     <div className={styles.getstarted}>Get Started</div>
                     <div className={styles.download}>Download</div>
@@ -22,7 +44,7 @@ const Hero: React.FC = () => {
                     className={styles.mainImage}
                     alt="Drona mobile app home screen"
                 />
-            </div>
+            </div> */}
         </div>
     )
 }
