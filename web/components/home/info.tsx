cc40a398dfd3c392a6af60a6bc900b8f85cffc7b
kumarshantanu01 <shantanuk280@gmail.com>
2021-11-12 22:37:40 +0530

    feat: ui enhanced and responsive

--- web/components/home/info.tsx
+++ web/components/home/info.tsx
@@ -19,21 +19,24 @@ const Infocard = (props: any) => {
 const Infocarddata = [
     {
         id: 1,
-        logo: 'https://res.cloudinary.com/dguy8qpzi/image/upload/v1636028569/create_a4ndl4.svg',
+        // logo: 'https://res.cloudinary.com/dguy8qpzi/image/upload/v1636028569/create_a4ndl4.svg',
+        logo: 'https://res.cloudinary.com/dguy8qpzi/image/upload/v1636736341/image-removebg-preview_22_00_onlwlf.png',
         Heading: 'Add your code',
         Subheading: 'Click a picture of the code written on the copy.',
     },
 
     {
         id: 2,
-        logo: 'https://res.cloudinary.com/dguy8qpzi/image/upload/v1636028569/add-content_djg0us.svg',
+        // logo: 'https://res.cloudinary.com/dguy8qpzi/image/upload/v1636028569/add-content_djg0us.svg',
+        logo: 'https://res.cloudinary.com/dguy8qpzi/image/upload/v1636736018/image-removebg-preview_20_pgz4hb.png',
         Heading: 'Upload',
         Subheading: 'Upload the file and select the language.',
     },
 
     {
         id: 3,
-        logo: 'https://res.cloudinary.com/dguy8qpzi/image/upload/v1636028564/launch_q0s5md.svg',
+        // logo: 'https://res.cloudinary.com/dguy8qpzi/image/upload/v1636028564/launch_q0s5md.svg',
+        logo: 'https://res.cloudinary.com/dguy8qpzi/image/upload/v1636736752/image-removebg-preview_27_lzp8at.png',
         Heading: 'Run',
         Subheading: 'Edit the code in the theme-based editor and run the code.',
     },
