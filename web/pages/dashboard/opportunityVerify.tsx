c2fd55e0a4e3d13ff7c05263059cfa08789dcafd
Sumana Basu <sumana.basu2001@gmail.com>
2021-11-14 10:41:44 +0530

    Feat: Opportunities dashboard added

--- web/pages/dashboard/opportunityVerify.tsx
+++ web/pages/dashboard/opportunityVerify.tsx
@@ -1,15 +1,29 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
+import Head from 'next/head'
+import styles from '../../styles/Dashboard.module.scss'
+import Link from 'next/link'
+
+function toApprove(id: string) {
+    try {
+        axios.put(`http://drona-ibm.herokuapp.com/api/opportunity/moderator/${id}`, { isApproved: 'true' }).then(function (response) {
+            // handle success
+            console.log(response)
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
 
 export default function OpportunityVerify() {
-    const [opportunity, setOpportunity] = useState<any[]>([])
+    const [opportunities, setOpportunities] = useState<any[]>([])
 
     useEffect(() => {
         if (localStorage.getItem('token')) {
             try {
-                axios.get(`http://drona-ibm.herokuapp.com/api/oportunity`).then(function (response) {
+                axios.get(`http://drona-ibm.herokuapp.com/api/opportunity`).then(function (response) {
                     // handle success
-                    setOpportunity(response.data)
+                    setOpportunities(response.data)
                 })
             } catch (error) {
                 console.log(error)
@@ -22,7 +36,43 @@ export default function OpportunityVerify() {
 
     return (
         <>
-            <h1>Opportunity Verify</h1>
+            <Head>
+                <title>Drona | Opportunities Verify</title>
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="apple-touch-icon" href="/256x256.png"></link>
+
+                <meta name="description" content="Empowering everyone to code with IBM's eye" />
+                <link rel="icon" href="/256x256.png" />
+            </Head>
+
+            <div>
+                {opportunities.map(opportunity => {
+                    return (
+                        <div key={opportunity._id}>
+                            <article className={styles.opportunity}>
+                                <div key={opportunity._id} className={styles.opportunityContent}>
+                                    <Link href={opportunity.url}>
+                                        <div>
+                                            <img src={opportunity.logo} alt="logo" height="150px" width="200px" />
+                                            <h1 className={styles.opportunityTitle} key={opportunity._id}>
+                                                {opportunity.role} ({opportunity.tag})
+                                            </h1>
+                                        </div>
+                                    </Link>
+                                    {opportunity.isApproved ? (
+                                        <button className={styles.btnSuccess}> ✅ Approved</button>
+                                    ) : (
+                                        <button className={styles.btnDanger} onClick={() => toApprove(opportunity._id)}>
+                                            {' '}
+                                            🙋🏻 Approve
+                                        </button>
+                                    )}
+                                </div>
+                            </article>
+                        </div>
+                    )
+                })}
+            </div>
         </>
     )
 }
