8ea494d29de0dac66ae179c44e076bfa45ab99c8
sumana2001 <sumana.basu2001@gmail.com>
2021-11-16 09:12:14 +0530

    Docs: Hero content added

--- web/components/home/hero.tsx
+++ web/components/home/hero.tsx
@@ -7,11 +7,10 @@ const Hero: React.FC = () => {
             <div className={styles.leftContainer}>
                 <h1 className={styles.heading}> Empowering everyone to code with IBMs eye</h1>
                 <p className={styles.subheading}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus velit eget magna tincidunt, nec sollicitudin neque
-                    ultrices. Sed lobortis eros sit amet venenatis accumsan. Integer sit amet sollicitudin purus. Proin fringilla iaculis
-                    felis in mollis. Donec suscipit iaculis neque, vel ultricies diam consequat non.
+                    Play around with code while we make sure you aren't lost! Join us and code laptop-free, stay updated and never miss an
+                    opportunity again and lastly be interview-ready with our crowdsourced question bank.
                 </p>
-                <div className={styles.buttons}>
+                <div className={styles.buttons} style={{ justifyItems: 'left' }}>
                     <div className={styles.demo}>Watch a demo</div>
                     <div className={styles.download}>Download APK</div>
                 </div>
@@ -25,26 +24,6 @@ const Hero: React.FC = () => {
                     alt="Drona mobile app home screen"
                 />
             </div>
-            {/* <div style={{ float: 'left' }}>
-                <h1 className={styles.heading}>
-                    {' '}
-                    Empowering everyone <br /> to code with IBM's eye
-                </h1>
-                <p className={styles.subheading}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus velit eget magna tincidunt,<br/> nec sollicitudin neque ultrices. Sed lobortis eros sit amet venenatis accumsan. Integer sit<br/> amet sollicitudin purus. Proin fringilla iaculis felis in mollis. Donec suscipit iaculis neque, vel<br/> ultricies diam consequat non. </p>
-                <div className={styles.buttons}>
-                    <div className={styles.getstarted}>Get Started</div>
-                    <div className={styles.download}>Download</div>
-                </div>
-            </div>
-            <div style={{ float: 'right' }}>
-                <Image
-                    src="https://res.cloudinary.com/dguy8qpzi/image/upload/v1636009473/drona-homeui_xqlfkq.png"
-                    width={380}
-                    height={700}
-                    className={styles.mainImage}
-                    alt="Drona mobile app home screen"
-                />
-            </div> */}
         </div>
     )
 }
